2eb6ebbe42ae5a835c4cc85cc43e729b707f6c43
Elijan Mastnak <ejmastnak@gmail.com>
2020-12-07 22:39:13 +0100

    spm results textarea

--- spm-interface.py
+++ spm-interface.py
@@ -1,9 +1,10 @@
 import matplotlib
-matplotlib.use("TkAgg")
+matplotlib.use("TkAgg")  # set tk backend
 from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from pathlib import Path
 import os
+import traceback
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
@@ -40,21 +41,32 @@ class MCModulationInterface:
         self.export_button = ttk.Button(self.controlframe, text="Export SPM", command=self.export)
         self.close_button = ttk.Button(self.controlframe, text="Exit", command=self.close)
 
+        # Baseline data window
         self.baseline_label = ttk.Label(self.controlframe, text="Baseline Data")
         self.baseline_scroll = ttk.Scrollbar(self.controlframe)
-        self.baseline_text = Text(self.controlframe, height=5, width=52)
-        self.baseline_scroll.config(command=self.baseline_text.yview)
-        self.baseline_text.config(yscrollcommand=self.baseline_scroll.set)
-        self.baseline_text.insert(END, "No data imported")
-        self.baseline_text.configure(state='disabled')
+        self.baseline_text_area = Text(self.controlframe, height=5, width=52)
+        self.baseline_scroll.config(command=self.baseline_text_area.yview)
+        self.baseline_text_area.config(yscrollcommand=self.baseline_scroll.set)
+        self.baseline_text_area.insert(END, "No data imported")
+        self.baseline_text_area.configure(state='disabled')
 
+        # Potentiated data window
         self.potentiated_label = ttk.Label(self.controlframe, text="Potentiated Data")
         self.potentiated_scroll = ttk.Scrollbar(self.controlframe)
-        self.potentiated_text = Text(self.controlframe, height=5, width=52)
-        self.potentiated_scroll.config(command=self.potentiated_text.yview)
-        self.potentiated_text.config(yscrollcommand=self.potentiated_scroll.set)
-        self.potentiated_text.insert(END, "No data imported")
-        self.potentiated_text.configure(state='disabled')
+        self.potentiated_text_area = Text(self.controlframe, height=5, width=52)
+        self.potentiated_scroll.config(command=self.potentiated_text_area.yview)
+        self.potentiated_text_area.config(yscrollcommand=self.potentiated_scroll.set)
+        self.potentiated_text_area.insert(END, "No data imported")
+        self.potentiated_text_area.configure(state='disabled')
+
+        # SPM analysis results window
+        self.spm_label = ttk.Label(self.controlframe, text="SPM Analysis Results")
+        self.spm_scroll = ttk.Scrollbar(self.controlframe)
+        self.spm_text_area = Text(self.controlframe, height=7, width=52)
+        self.spm_scroll.config(command=self.spm_text_area.yview)
+        self.spm_text_area.config(yscrollcommand=self.spm_scroll.set)
+        self.spm_text_area.insert(END, "No analysis results")
+        self.spm_text_area.configure(state='disabled')
 
         # # create signal graph
         # self.signal_fig = Figure(figsize=(6, 2), dpi=100) # figure to hold graph of muscle signals
@@ -82,17 +94,21 @@ class MCModulationInterface:
         # gridding control panel
         self.baseline_label.grid(column=0, row=1)
         self.baseline_scroll.grid(column=1, row=2, sticky=W)
-        self.baseline_text.grid(column=0, row=2, sticky=W)
+        self.baseline_text_area.grid(column=0, row=2, sticky=W)
 
         self.potentiated_label.grid(column=0, row=3)
         self.potentiated_scroll.grid(column=1, row=4, sticky=W)
-        self.potentiated_text.grid(column=0, row=4, sticky=W)
+        self.potentiated_text_area.grid(column=0, row=4, sticky=W)
 
-        self.import_baseline_button.grid(column=0, row=5, sticky=W)
-        self.import_potentiated_button.grid(column=0, row=6, sticky=W)
-        self.compare_button.grid(column=0, row=7, sticky=W)
-        self.export_button.grid(column=0, row=8, sticky=W)
-        self.close_button.grid(column=0, row=9, sticky=W)
+        self.spm_label.grid(column=0, row=5)
+        self.spm_scroll.grid(column=1, row=6, sticky=W)
+        self.spm_text_area.grid(column=0, row=6, sticky=W)
+
+        self.import_baseline_button.grid(column=0, row=7, sticky=W)
+        self.import_potentiated_button.grid(column=0, row=8, sticky=W)
+        self.compare_button.grid(column=0, row=9, sticky=W)
+        self.export_button.grid(column=0, row=10, sticky=W)
+        self.close_button.grid(column=0, row=11, sticky=W)
 
         # gridding graphs
         # self.signal_canvas.get_tk_widget().grid(column=0, row=0, sticky=(N, S, E, W))
@@ -109,7 +125,7 @@ class MCModulationInterface:
         self.root.mainloop()
 
     @ staticmethod
-    def get_data_description_string(filename, data):
+    def get_data_description(filename, data):
         """
         Used to get a string description of imported baseline/potentiated data
         to display in the GUI, so the user knows what they've imported
@@ -119,27 +135,40 @@ class MCModulationInterface:
         :return:
         """
 
-        if len(data.shape) == 2:  # a matrix
-            file_string = "Data: " + Path(filename).name
-            dim_string_1 = str("Dimensions: (" + str(data.shape[0]) + " x " + str(data.shape[1]) + ")")
-            dim_string_2 = "(" + str(data.shape[1]) + " measurements of " + str(data.shape[0]) + " points per measurement)"
-            path_string = "Location: " + os.path.dirname(filename)
-            return file_string + "\n" + dim_string_1 + "\n" + dim_string_2 + "\n" + path_string
-
-        elif len(data.shape) == 1:  # a single column
-            file_string = "Data: " + Path(filename).name
-            dim_string_1 = str("Dimensions: (" + str(data.shape[0]) + " x " + str(1) + ")")
-            dim_string_2 = "(1 measurements of " + str(data.shape[0]) + " points per measurement)"
-            path_string = "Location: " + os.path.dirname(filename)
-            return file_string + "\n" + dim_string_1 + "\n" + dim_string_2 + "\n" + path_string
-
-        else: return "Error importing data.\nData has wrong shape: " + str(data.shape)
+        file_string = "Filename: " + Path(filename).name
+        dim_string_1 = "Dimensions: {} rows by {} columns".format(data.shape[0], data.shape[1])
+        dim_string_2 = "({} measurements of {} points per measurement)".format(data.shape[1], data.shape[0])
+        path_string = "Location: " + os.path.dirname(filename)
+        return file_string + "\n" + dim_string_1 + "\n" + dim_string_2 + "\n" + path_string
 
     @ staticmethod
-    def set_text_content_description_after_import(text_widget, text_content):
+    def get_spm_description_string(ti):
+        """
+        Used to get a string description of an SPM two-sample t-test
+
+        :param ti: An SPM TI inference object
+        :return:
+        """
+        analysis_string = "Alpha: {:.2f}".format(ti.alpha)  # alpha value
+        analysis_string += "\nThreshold: {:.2f}".format(ti.zstar)  # threshold t-statistic value
+        clusters = ti.clusters  # portions of curve above threshold value
+        if clusters is not None:
+            for i, cluster in enumerate(clusters):
+                print(cluster.centroid)
+                print(cluster.endpoints[0])
+                cluster_string = "\n\nSignificance Region {}".format(i+1)  # include a newline character
+                cluster_string += "\nProbability: {:.2e} = {:.4f}".format(cluster.P, cluster.P)
+                cluster_string += "\nStart: {:.2f}\t End: {:.2f}".format(cluster.endpoints[0], cluster.endpoints[1])
+                cluster_string += "\nCentroid: ({:.2f}, {:.2f})".format(cluster.centroid[0], cluster.centroid[1])
+                analysis_string += cluster_string
+        return analysis_string
+
+    @ staticmethod
+    def set_import_description(text_widget, text_content):
         """
         Wrapper method for protocol of setting data text widget info content
-        The idea is to give the user a description of the data they've imported
+        Used with to give the user a description of the data they've imported
+        And to give an overview of SPM analysis results
         :param text_widget:
         :param text_content:
         :return:
@@ -161,14 +190,12 @@ class MCModulationInterface:
         if filename:  # only called if a file is chosen and avoids null filename on cancel click
             try:
                 self.baseline_data = np.loadtxt(filename, delimiter=",", skiprows=self.start_row, max_rows=self.max_rows)  # load data
-                self.baseline_data = self.baseline_data + self.position_offset
-                if self.normalize: self.baseline_data = self.baseline_data / self.baseline_data.max(axis=0)
+                self.process_baseline_data()  # process imported data
+                self.set_import_description(self.baseline_text_area, self.get_data_description(filename, self.baseline_data))
 
-                self.set_text_content_description_after_import(self.baseline_text, self.get_data_description_string(filename, self.baseline_data))
-                if len(self.baseline_data.shape) == 1: # turn a 1D array into a 2D matrix with one column
-                    self.baseline_data = self.baseline_data.reshape(-1, 1)
             except Exception as e:
                 print("Error importing data: " + str(e))
+                traceback.print_tb(err.__traceback__)
                 return
 
     def import_potentiated(self):
@@ -177,15 +204,12 @@ class MCModulationInterface:
         Implements protocol for importing potentiated data
         """
         filename = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])  # get csv files
-        if filename: # only called if a file is chosen and avoids null filename on cancel click
+        if filename:  # only called if a file is chosen and avoids null filename on cancel click
             try:
                 self.pot_data = np.loadtxt(filename, delimiter=",", skiprows=self.start_row, max_rows=self.max_rows)  # load data
-                self.pot_data = self.pot_data + self.position_offset
-                if self.normalize: self.pot_data = self.pot_data / self.pot_data.max(axis=0)
+                self.process_potentiated_data()  # process imported data
+                self.set_import_description(self.potentiated_text_area, self.get_data_description(filename, self.pot_data))
 
-                self.set_text_content_description_after_import(self.potentiated_text, self.get_data_description_string(filename, self.pot_data))
-                if len(self.pot_data.shape) == 1: # turn a 1D array into a 2D matrix with one column
-                    self.pot_data = self.pot_data.reshape(-1, 1)
             except Exception as e:
                 print("Error importing data: " + str(e))
                 return
@@ -260,6 +284,31 @@ class MCModulationInterface:
     # -----------------------------------------------------------------------------
     # START SPM SIGNAL PROCESSING FUNCTIONS
     # -----------------------------------------------------------------------------
+    def process_baseline_data(self):
+        if self.baseline_data is None:  # null check
+            return
+
+        if self.normalize: self.baseline_data = self.baseline_data / self.baseline_data.max(axis=0)  # normalize
+        self.baseline_data = self.baseline_data + self.position_offset  # add vertical offset
+
+        self.baseline_data = self.baseline_data + self.position_offset
+
+        # if data is a single column (1D array) reshape into a 2D array (matrix with one column)
+        if len(self.baseline_data.shape) == 1:
+            self.baseline_data = self.baseline_data.reshape(-1, 1)
+
+    def process_potentiated_data(self):
+        if self.pot_data is None:  # null check
+            return
+
+        if self.normalize: self.pot_data = self.pot_data / self.pot_data.max(axis=0)  # normalize
+        self.pot_data = self.pot_data + self.position_offset  # add vertical offset
+
+        self.pot_data = self.pot_data + self.position_offset
+
+        # if data is a single column (1D array) reshape into a 2D array (matrix with one column)
+        if len(self.pot_data.shape) == 1:
+            self.pot_data = self.pot_data.reshape(-1, 1)
 
     # -----------------------------------------------------------------------------
     # END SPM SIGNAL PROCESSING FUNCTIONS
@@ -374,8 +423,7 @@ class MCModulationInterface:
         else:
             return False
 
-    @ staticmethod
-    def run_two_sample_test(base_data, pot_data, x_label="Independent Variable", y_label="Dependent Variable"):
+    def run_two_sample_test(self, base_data, pot_data, x_label="Independent Variable", y_label="Dependent Variable"):
         """
         Runs and plots the results of a two-sample SPM t test between base_data and pot_data.
         Plot results in an external window using SPM's plotting utilities
@@ -387,19 +435,21 @@ class MCModulationInterface:
         """
         try:
             t = spm1d.stats.ttest2(pot_data, base_data, equal_var=False)
-
             ti = t.inference(alpha=0.05, two_tailed=False, interp=True)
         except Exception as e:
             print("Error performing SPM analysis: " + str(e))
             return
 
+        self.set_import_description(self.spm_text_area, self.get_spm_description_string(ti))
         #  Plot:
         plt.close('all')
         # plot mean and SD:
         plt.figure(figsize=(8, 3.5))
         ax = plt.axes((0.1, 0.15, 0.35, 0.8))
+
         spm1d.plot.plot_mean_sd(pot_data, label="Potentiated", linecolor='r', facecolor='r')
         spm1d.plot.plot_mean_sd(base_data, label="Baseline")
+
         ax.axhline(y=0, color='k', linestyle=':')
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
@@ -408,8 +458,8 @@ class MCModulationInterface:
         # plot SPM results:
         ax = plt.axes((0.55, 0.15, 0.35, 0.8))
         ti.plot()
-        ti.plot_threshold_label(fontsize=10, color='black')
-        ti.plot_p_values(size=10)  # offsets=[(0, 0.3)]
+        # ti.plot_threshold_label(fontsize=10, color='black')
+        # ti.plot_p_values(size=10)  # offsets=[(0, 0.3)]
         ax.set_xlabel(x_label)
         plt.show()
 
