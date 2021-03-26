abe600aa572b09eb10467546f5d6816234dedc60
Elijan Mastnak <ejmastnak@gmail.com>
2021-03-26 14:37:08 +0100

    improved conversion options from excel to csv

--- spm_io.py
+++ spm_io.py
@@ -31,7 +31,7 @@ def make_output_dir(base_dir_name):
         return base_dir_name + "_" + str(counter) + "/"
 
 
-def convert_directory(directory, base_reps=1, pot_reps=1):
+def convert_directory(directory, conversion_mode, base_reps=1, pot_reps=1):
     """
     Input the path to a directory containing TMG-format excel files
     Converts all ".xlsx" files in the specified directory to csv files
@@ -41,8 +41,15 @@ def convert_directory(directory, base_reps=1, pot_reps=1):
     for filename in sorted(os.listdir(directory)):
         if ".xlsx" in filename and "$" not in filename:
             print(filename)
-            # excel_to_csv(directory, filename, output_dir)
-            excel_to_csv_base_pot(directory, filename, output_dir, base_reps=base_reps, pot_reps=pot_reps)
+            if conversion_mode == EXCEL_TO_CSV:
+                excel_to_csv(directory, filename, output_dir)
+            elif conversion_mode == EXCEL_TO_CSV_BASE_POT:
+                excel_to_csv_base_pot(directory, filename, output_dir, base_reps=base_reps, pot_reps=pot_reps)
+            elif conversion_mode == EXCEL_TO_CSV_BASE_POT_SET_FILES:
+                excel_to_csv_base_pot_set_files(directory, filename, output_dir, base_reps=base_reps, pot_reps=pot_reps)
+            else:
+                print("Aborting. Invalid conversion mode.")
+                return
 
 
 def excel_to_csv(parent_dir, filename, output_dir):
@@ -99,8 +106,8 @@ def excel_to_csv_base_pot(parent_dir, filename, output_dir, base_reps=1, pot_rep
     else:
         sets = int(sets)
 
-    base_cols = []  # construct column numbers of all baseline files
-    pot_cols = []  # construct column numbers of all baseline files
+    base_cols = []  # construct column numbers of all baseline measurements in the entire Excel file
+    pot_cols = []  # construct column numbers of all baseline measurements in the entire Excel file
     for s in range(sets):
         for r in range(base_reps):
             base_cols.append(s * reps_per_set + r + 1)
@@ -112,25 +119,77 @@ def excel_to_csv_base_pot(parent_dir, filename, output_dir, base_reps=1, pot_rep
     print("Potentiated column numbers: ", end="")
     print(pot_cols)
 
-    df.to_csv(output_dir + filename.replace(".xlsx", "_base.csv"), header=False, index=False, columns=base_cols)
-    df.to_csv(output_dir + filename.replace(".xlsx", "_pot.csv"), header=False, index=False, columns=pot_cols)
+    df.to_csv(output_dir + filename.replace(".xlsx", "-base.csv"), header=False, index=False, columns=base_cols)
+    df.to_csv(output_dir + filename.replace(".xlsx", "-pot.csv"), header=False, index=False, columns=pot_cols)
 
 
-def do_conversion():
-    folder = "klanec-17-03-2021/"
-    base_reps = 1
-    pot_reps = 1
-    convert_directory(data_dir + folder, base_reps=base_reps, pot_reps=pot_reps)
+def excel_to_csv_base_pot_set_files(parent_dir, filename, output_dir, base_reps=1, pot_reps=1):
+    """
+    Input: path to an TMG-format Excel file, wihth columns in the format
+        base_reps baseline measurements
+        pot_reps  potentiated measurements
+        base_reps baseline measurements
+        pot_reps  potentiated measurements
+        ... etc...
+        base_reps baseline measurements
+        pot_reps  potentiated measurements  # end with potentiated measurement
+
+    One group of base_reps + pot_reps measurements constitutes one set
+
+    Output: Many CSV files (2x the number of sets in the inputed Excel file)
+     One baseline CSV file for each measurement set
+     One potentiate CSV file for each measurement set
+
+    :param parent_dir: parent directory containing the xlsx file  e.g. "Users/user/tmg-data/measurements/"
+    :param filename: the name of the xlsx file, including extension e.g. "EM1234.xlsx"
+    :param output_dir: the parent output directory for the folders containing the converted CSV files
+    :param base_reps: number of measurements per each baseline state
+    :param pot_reps: number of measurements per each potentiated state
+    """
+    path = parent_dir + filename
+
+    # read excel and drop first column, which is empty in TMG format
+    df = pd.read_excel(path, engine='openpyxl', header=None, skiprows=DATA_START_ROW).drop(columns=[0])
+
+    n_cols = df.shape[1]  # number of total measurements in Excel file
+    reps_per_set = base_reps + pot_reps
+    sets = n_cols/reps_per_set  # compute number of measurement sets in Excel file
+
+    if not sets.is_integer():
+        print("Error: Inputed repetition values lead to a non-integer number of sets.")
+        print("Aborting")
+        return
+    else:
+        sets = int(sets)
+
+    output_dir = make_output_dir(output_dir + filename.replace(".xlsx", ""))  # create an output directory for each file
+    for s in range(sets):
+        print("Set Number: {}".format(s))
+        base_cols = []  # construct column numbers of baseline measurements in the current set
+        pot_cols = []  # construct column numbers of all baseline measurements in the current set
+        for r in range(base_reps):
+            base_cols.append(s * reps_per_set + r + 1)
+        for r in range(pot_reps):
+            pot_cols.append(s * reps_per_set + r + 1 + base_reps)
+
+        print("Baseline column numbers: ", end="")
+        print(base_cols)
+        print("Potentiated column numbers: ", end="")
+        print(pot_cols)
+        print()
+
+        df.to_csv(output_dir + filename.replace(".xlsx", "-base-{}.csv".format(s + 1)), header=False, index=False, columns=base_cols)
+        df.to_csv(output_dir + filename.replace(".xlsx", "-pot-{}.csv".format(s + 1)), header=False, index=False, columns=pot_cols)
 
 
-def test_cols():
-    dir = "/Users/ejmastnak/Documents/Media/tmg-bmc-media/measurements/potenciacija-19-03-2021/"
-    path = dir + "test.xlsx"
-    df = pd.read_excel(path, engine='openpyxl', header=None, skiprows=1)
-    print(df.head())
-    df = df.drop(columns=[0])
-    print("Dropped")
-    print(df.head())
+def conversion_wrapper():
+    """ Wrapper method for running a conversion """
+    # folder = "klanec-17-03-2021/"
+    convert_dir = "/Users/ejmastnak/Documents/Media/tmg-bmc-media/measurements/potenciacija-19-03-2021/NF-squat/"
+    base_reps = 8
+    pot_reps = 8
+    convert_directory(convert_dir, EXCEL_TO_CSV_BASE_POT, base_reps=base_reps, pot_reps=pot_reps)
+    # convert_directory(convert_dir, EXCEL_TO_CSV_BASE_POT_SET_FILES, base_reps=base_reps, pot_reps=pot_reps)
 
 
 DATA_START_ROW = 24
@@ -138,10 +197,14 @@ DATA_START_COL = "B"
 # DATA_END_COL = "Q"
 DATA_END_COL = "DY"
 
+EXCEL_TO_CSV = 1  # take the measurements from a TMG excel file and put them verbatim in a CSV file. Do not separate baseline or potentiated
+EXCEL_TO_CSV_BASE_POT = 2  # save all baseline measurements in a TMG excel file in one CSV file and all potentiated measurements in a separate CSV file.
+EXCEL_TO_CSV_BASE_POT_SET_FILES = 3  # create separate CSV files for the baseline and potentiated measurements in each set of measurement in the TMG excel file
+
 
 if __name__ == "__main__":
     # read_tmg_excel()
-    do_conversion()
+    conversion_wrapper()
     # test_cols()
 
 # TODO: customizable data end column "DY"
